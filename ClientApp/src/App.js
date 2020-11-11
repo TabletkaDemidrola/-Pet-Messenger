469cadbaa37f23928ebbbddb33f4bd56c26a831a
Selena <kuznetzova.a98@mail.ru>
2020-11-11 20:19:33 +0700

    create Front

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
-import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
-import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
+import React from 'react'
+import Servers from './components/Servers/Servers'
+import Sidebare from './components/Sidebare/Sidebare'
+import Chat from './components/Chat/Chat'
+import Profile from './components/Profile/Profile'
+import Login from './components/Login/Login'
+import './App.css'
 
-import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+function App() {
+    const user = false
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
-    );
-  }
+        <div className="app">
+            {!user 
+            ?
+                <>
+                    <Servers />
+                    <Sidebare />
+                    <Chat />
+                    <Profile /> 
+                </>
+            :
+                <>
+                    <Login />
+                </>
+            }
+        </div>
+    )
 }
+
+export default App
---
+++ ClientApp/src/components/Servers/Server/Server.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Server.css'
+import { Avatar } from '@material-ui/core'
+
+function Server() {
+    return (
+        <div className="server">
+            <div className="server__active"></div>
+            <Avatar>H</Avatar>
+        </div>
+    )
+}
+
+export default Server
