85383926a0a1182677ef1465b260de4f0fa53d29
Selena <kuznetzova.a98@mail.ru>
2020-11-18 20:20:34 +0700

    tooltip for friends list

--- ClientApp/src/components/General/Friends/FriendsList/Friend/Friend.js
+++ ClientApp/src/components/General/Friends/FriendsList/Friend/Friend.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Friend.css'
 import { Avatar } from '@material-ui/core'
-import SmsIcon from '@material-ui/icons/Sms';
+import SmsIcon from '@material-ui/icons/Sms'
+import CloseIcon from '@material-ui/icons/Close'
+import Tooltip from '@material-ui/core/Tooltip'
 
 function Friend() {
     return (
@@ -9,7 +11,12 @@ function Friend() {
             <div className="friend__status friend__status--online"></div>
             <Avatar />
             <h4 className="friend__name">Friend Name</h4>
-            <SmsIcon className="friend__send"/>
+            <Tooltip className="friend__action" title="Message" arrow placement="top">
+                <SmsIcon />
+            </Tooltip>
+            <Tooltip className="friend__action" title="Remove from friends" arrow placement="top">
+                <CloseIcon />
+            </Tooltip>
         </div>
     )
 }
