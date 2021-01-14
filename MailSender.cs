a5e5a06cefd99608acc1968f95fbdb020d72d674
HiddenGod <shefer-artem@mail.ru>
2021-01-14 13:05:33 +0700

    Edit MailSender

--- MailSender.cs
+++ MailSender.cs
@@ -19,43 +19,19 @@ namespace Messenger
         {
             Options = optionsAccessor.Value;
         }
-
         public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
-        #region SendGrid
-        /*
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
-            return Execute(Options.SendGridKey, subject, message, email);
-        }
-
-        public Task Execute(string apiKey, string subject, string message, string email)
-        {
-            var client = new SendGridClient(apiKey);
-            var msg = new SendGridMessage()
-            {
-                From = new EmailAddress("Joe@contoso.com", Options.SendGridUser),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
-            };
-            msg.AddTo(new EmailAddress(email));
-            msg.SetClickTracking(false, false);
-
-            return client.SendEmailAsync(msg);
-        }*/
-        #endregion
-
+        // To compile this file in your project, you need create class MailSetting self
         public async Task SendEmailAsync(string email, string subject, string message)
         {
-            MailAddress from = new MailAddress("wtf.fail.omg.hc@gmail.com", "Artem Shefer");
+            MailAddress from = new MailAddress(MailSetting.Email, MailSetting.DisplayName);
             MailAddress to = new MailAddress(email);
             MailMessage m = new MailMessage(from, to);
             m.Subject = subject;
             m.Body = message;
             m.IsBodyHtml = true;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new NetworkCredential("wtf.fail.omg.hc@gmail.com", "lolkaArTeM321456");
+            smtp.Credentials = new NetworkCredential(MailSetting.Email, MailSetting.Password);
             smtp.EnableSsl = true;
             await smtp.SendMailAsync(m);
         }
